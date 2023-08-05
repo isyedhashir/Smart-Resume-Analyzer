9cb2dc0303c9028143a2f39c5fdce0d487ccc132
isyedhashir@gmail.com <DELL@DESKTOP-7C4357L>
2023-08-05 12:15:04 -0500

    Funtion pdf has been added.

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
-import time
+import time,datetime
+#libraries to parse the resume pdf files
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
 from streamlit_tags import st_tags
 from PIL import Image
 import pymysql
-import mysql.connector
 
 
 st.set_page_config(
@@ -13,39 +19,61 @@ st.set_page_config(
 img = Image.open('./Logo/images (2).png')
 # resized_img = img.resize((500, 500))
 
-img = Image.open('./Logo/images (2).png')
-resized_img = img.resize((800, 600))
-st.title("Smart Resume Analyzer")
-st.sidebar.markdown("# Choose User")
-activities = ["User", "Admin"]
-choice = st.sidebar.selectbox("Choose among the given options:", activities)
-
-if choice == 'User':
-    st.markdown('''<h5 style='text-align: left; color: #021659;'> "Unlock the power of your resume! Upload your document and unlock a world of smart recommendations."</h5>''', unsafe_allow_html=True)
-    pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
-    if pdf_file is not None:
-        with st.spinner('Uploading your Resume...'):
-            time.sleep(4)
-        save_image_path = './Uploaded_Resumes/' + pdf_file.name
 
 
-# # Connect to the database
-# connection = pymysql.connect(host='localhost',
-#                              user='root',
-#                              password='local',
-#                              database='db',
-#                              cursorclass=pymysql.cursors.DictCursor)
+
+#CONNECT TO DATABASE
 
 connection = pymysql.connect(host='localhost',user='root',password='local',db='db')
 cursor = connection.cursor()
 
+def insert_data(name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses):
+    DB_table_name = 'user_data'
+    insert_sql = "insert into " + DB_table_name + """
+    values (0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    rec_values = (name, email, str(res_score), timestamp,str(no_of_pages), reco_field, cand_level, skills,recommended_skills,courses)
+    cursor.execute(insert_sql, rec_values)
+    connection.commit()
 
-db_sql = """CREATE DATABASE IF NOT EXISTS db;"""
-cursor.execute(db_sql)
+
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+
+
+def run():
+    img = Image.open('./Logo/images (2).png')
+    # img = img.resize((250,250))
+    st.image(img)
+    st.title("AI Resume Analyser")
+    activities = ["User"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+    
+
+
+    # Create the DB
+    db_sql = """CREATE DATABASE IF NOT EXISTS db;"""
+    cursor.execute(db_sql)
 
     # Create table
-DB_table_name = 'user_data'
-table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
+    DB_table_name = 'user_data'
+    table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
                     (ID INT NOT NULL AUTO_INCREMENT,
                      Name varchar(500) NOT NULL,
                      Email_ID VARCHAR(500) NOT NULL,
@@ -59,9 +87,22 @@ table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
                      Recommended_courses BLOB NOT NULL,
                      PRIMARY KEY (ID));
                     """
-cursor.execute(table_sql)
+    cursor.execute(table_sql)
+    if choice == 'User':
+        st.markdown('''<h5 style='text-align: left; color: #021659;'> Upload your resume, and get smart recommendations</h5>''',
+                    unsafe_allow_html=True)
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            with st.spinner('Uploading your Resume...'):
+                time.sleep(4)
+            os.makedirs("Uploaded_Resumes", exist_ok=True)
 
+            save_image_path = './Uploaded_Resumes/' + pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
 
-
-
-
+            
+    
+run()
