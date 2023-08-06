4bc650e26b645cee75a0dbf6f980d347bad07a9a
isyedhashir@gmail.com <DELL@DESKTOP-7C4357L>
2023-08-06 13:45:22 -0500

    Fuction Show_PDF Implemented to show PDF content.

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import streamlit as st
-import time,datetime
-#libraries to parse the resume pdf files
-from pyresparser import ResumeParser
-from pdfminer3.layout import LAParams, LTTextBox
-from pdfminer3.pdfpage import PDFPage
-from pdfminer3.pdfinterp import PDFResourceManager
-from pdfminer3.pdfinterp import PDFPageInterpreter
-from pdfminer3.converter import TextConverter
+import time
 from streamlit_tags import st_tags
 from PIL import Image
 import pymysql
+import mysql.connector
 
 
 st.set_page_config(
@@ -27,49 +21,9 @@ img = Image.open('./Logo/images (2).png')
 connection = pymysql.connect(host='localhost',user='root',password='local',db='db')
 cursor = connection.cursor()
 
-def insert_data(name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses):
-    DB_table_name = 'user_data'
-    insert_sql = "insert into " + DB_table_name + """
-    values (0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-    rec_values = (name, email, str(res_score), timestamp,str(no_of_pages), reco_field, cand_level, skills,recommended_skills,courses)
-    cursor.execute(insert_sql, rec_values)
-    connection.commit()
 
-
-
-def pdf_reader(file):
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
-    with open(file, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            page_interpreter.process_page(page)
-            print(page)
-        text = fake_file_handle.getvalue()
-
-    # close open handles
-    converter.close()
-    fake_file_handle.close()
-    return text
-
-
-
-def run():
-    img = Image.open('./Logo/images (2).png')
-    # img = img.resize((250,250))
-    st.image(img)
-    st.title("AI Resume Analyser")
-    activities = ["User"]
-    choice = st.sidebar.selectbox("Choose among the given options:", activities)
-    
-
-
-    # Create the DB
-    db_sql = """CREATE DATABASE IF NOT EXISTS db;"""
-    cursor.execute(db_sql)
+db_sql = """CREATE DATABASE IF NOT EXISTS db;"""
+cursor.execute(db_sql)
 
     # Create table
     DB_table_name = 'user_data'
@@ -97,12 +51,7 @@ def run():
                 time.sleep(4)
             os.makedirs("Uploaded_Resumes", exist_ok=True)
 
-            save_image_path = './Uploaded_Resumes/' + pdf_file.name
-            with open(save_image_path, "wb") as f:
-                f.write(pdf_file.getbuffer())
-            show_pdf(save_image_path)
-            resume_data = ResumeParser(save_image_path).get_extracted_data()
 
-            
-    
-run()
+
+
+
